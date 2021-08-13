2d7068549e47a4d65f8a23f49f0121154a8e75db
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-08-13 22:28:21 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		fmt.Println("Error Running Server")
+	}
+
+}
